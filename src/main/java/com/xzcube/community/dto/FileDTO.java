ab2f12cccc9aed1ebe1cea66d7d0b2dc819e5394
xzcube <1482240153@qq.com>
2021-06-06 23:42:14 +0800

    添加图片上传功能

---
+++ src/main/java/com/xzcube/community/dto/FileDTO.java
@@ -0,0 +1,14 @@
+package com.xzcube.community.dto;
+
+import lombok.Data;
+
+/**
+ * @author xzcube
+ * @date 2021/6/6 21:31
+ */
+@Data
+public class FileDTO {
+    int success;
+    String message;
+    String url;
+}
