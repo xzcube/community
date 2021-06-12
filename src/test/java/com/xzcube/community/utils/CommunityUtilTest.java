cbccb83521c09b3e9bedd8b9d3238f706808a736
xzcube <1482240153@qq.com>
2021-06-13 00:27:34 +0800

    远程部署

--- src/test/java/com/xzcube/community/utils/CommunityUtilTest.java
+++ src/test/java/com/xzcube/community/utils/CommunityUtilTest.java
@@ -1,15 +1,9 @@
 package com.xzcube.community.utils;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.xzcube.community.model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author xzcube
